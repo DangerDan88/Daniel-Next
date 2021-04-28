2289fd7a3dfb629069e5e38e7fd232dbc9dd7c80
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-04-28 11:24:58 -0600

    working on oulling data from api getting 404 error on get request

--- personal-site/components/WeatherWidget.js
+++ personal-site/components/WeatherWidget.js
@@ -1,19 +1,18 @@
 import { useEffect } from "react";
-//Todo need to figure out api call not getting a response check api docs api key will be activated in a few hours
+import axios from "axios";
+
+//todo api request failing with 404 I think it has to do with the url having local host attached works in postman without that local host part
+
 const WeatherWidget = () => {
   useEffect(() => {
-    fetch(
-      "api.openweathermap.org/data/2.5/weather?q=London&appid={process.env.API_KEY}"
+    axios(
+      "api.openweathermap.org/data/2.5/weather?q=London&appid=127c4e3c16ae1e9d22ad527bfe38537a"
     )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          console.log(result.data);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   return (
